8d224850fb1bfc38fdaaee7dec0dad8c5f5cd6c3
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-03-29 09:45:54 +0200

    Add files via upload

---
+++ UpotrebaKlaseDirectory1-EM/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+namespace DirectoryKlasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite putanju direktorija:");
+            string putanja = Console.ReadLine();
+            // Provjeravamo postoji li uneseni direktorij
+            if (Directory.Exists(putanja))
+            {
+                // Ispisujemo sve datoteke u direktoriju
+                Console.WriteLine("Datoteke:");
+                foreach (string datoteka in Directory.
+                GetFiles(putanja))
+                {
+                    Console.WriteLine(datoteka);
+                }
+                // Provjeravamo postoji li poddirektorij TEST
+                string putanjaPoddirektorija = putanja + "\\TEST";
+
+
+
+                if (!Directory.Exists(putanjaPoddirektorija))
+                {
+                    // Ako ne postoji, kreiramo ga
+                    Directory.CreateDirectory(putanjaPoddirektorija);
+                }
+                // Ispisujemo sve poddirektorije
+                Console.WriteLine("Poddirektoriji:");
+                foreach (string poddirektorij
+                in Directory.GetDirectories(putanja))
+                {
+                    Console.WriteLine(poddirektorij);
+                }
+                // Brišemo poddirektorij TEST
+                Directory.Delete(putanjaPoddirektorija);
+                // Ponovno ispisujemo sve poddirektorije
+                Console.WriteLine("Nakon brisanja direktorija TEST:");
+                foreach (string poddirektorij
+                in Directory.GetDirectories(putanja))
+                {
+                    Console.WriteLine(poddirektorij);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Putanja ne postoji na računalu!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
